0f5b9196f16215105bb23ca735fde486a5cc930a
Kazi Mamun <kazimamun716@gmail.com>
2022-08-03 15:23:20 +0600

    delete modal added

--- src/components/Dashboard/UserOrders.js
+++ src/components/Dashboard/UserOrders.js
@@ -1,41 +1,48 @@
 import { useQuery } from "@tanstack/react-query";
-import React from "react";
+import React, { useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
 import auth from "../../firebase.init";
-import Loading from '../shared/Loading/Loading';
+import DeleteConfirmation from "../shared/DeleteConfirmation/DeleteConfirmation";
+import Loading from "../shared/Loading/Loading";
 
 const UserOrders = () => {
   const [user] = useAuthState(auth);
-  const { data: orders, refetch, isLoading } = useQuery(["orders"], () =>
-    fetch(`https://powerful-oasis-61993.herokuapp.com/mf/orders/${user?.email}`,{
-      headers:{
-        authorization: `Bearer ${localStorage.getItem('accessToken')}`
+  const [openDeleteModal, setOpenDeleteModal] = useState({});
+  const {
+    data: orders,
+    refetch,
+    isLoading,
+  } = useQuery(["orders"], () =>
+    fetch(
+      `https://powerful-oasis-61993.herokuapp.com/mf/orders/${user?.email}`,
+      {
+        headers: {
+          authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
       }
-    }).then((res) =>
-      res.json()
-    )
+    ).then((res) => res.json())
   );
-  const handleDelete =id=>{
-    fetch(`https://powerful-oasis-61993.herokuapp.com/mf/orders/${id}`,{
-        method: 'DELETE',
-        headers:{
-          authorization: `Bearer ${localStorage.getItem('accessToken')}`
-        }
+  const handleDelete = (id) => {
+    fetch(`https://powerful-oasis-61993.herokuapp.com/mf/orders/${id}`, {
+      method: "DELETE",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
     })
-    .then(res=>res.json())
-    .then(result=>{
-        if(result.deletedCount>0){
-            toast.success('Your Order is Deleted');
-            refetch();
-        }else{
-            toast.error('something wrong')
+      .then((res) => res.json())
+      .then((result) => {
+        if (result.deletedCount > 0) {
+          toast.success("Your Order is Deleted");
+          refetch();
+        } else {
+          toast.error("something wrong");
         }
-    })
-  }
-  if(isLoading){
-    return <Loading />
+      });
+  };
+  if (isLoading) {
+    return <Loading />;
   }
   return (
     <div>
@@ -54,19 +61,40 @@ const UserOrders = () => {
             </tr>
           </thead>
           <tbody>
-            {
-                orders?.map((order, index)=><tr key={order._id}>
-                    <th>{index+1}</th>
-                    <td>{order.serviceName}</td>
-                    <td>{order.user}</td>
-                    <td>{order.qty}</td>
-                    <td>${order.price}</td>
-                    <td><Link className="btn btn-xs btn-secondary" to='/dashboard/payment'>Pay</Link></td>
-                    <td><button onClick={()=>handleDelete(order._id)} className="btn btn-xs">Delete</button></td>
-                  </tr>)
-            }
+            {orders?.map((order, index) => (
+              <tr key={order._id}>
+                <th>{index + 1}</th>
+                <td>{order.serviceName}</td>
+                <td>{order.user}</td>
+                <td>{order.qty}</td>
+                <td>${order.price}</td>
+                <td>
+                  <Link
+                    className="btn btn-xs btn-secondary"
+                    to="/dashboard/payment"
+                  >
+                    Pay
+                  </Link>
+                </td>
+                <td>
+                  <label
+                    htmlFor="delete-modal"
+                    onClick={() => setOpenDeleteModal(order)}
+                    className="btn btn-xs btn-warning"
+                  >
+                    Delete
+                  </label>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
+        {openDeleteModal && (
+          <DeleteConfirmation
+            order={openDeleteModal}
+            handleDelete={handleDelete}
+          />
+        )}
       </div>
     </div>
   );
---
+++ src/components/shared/DeleteConfirmation/DeleteConfirmation.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const DeleteConfirmation = ({order, handleDelete}) => {
+  return (
+    <div>
+      <input type="checkbox" id="delete-modal" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Do you want to delete <span className="text-primary">{order.serviceName}</span>?</h3>
+          <p className="py-4">
+            If you click on <span className="text-red-500">Confirm Delete</span> your product will delete permanently
+          </p>
+          <div className="modal-action">
+            <label htmlFor="delete-modal" className="btn btn-accent text-white font-bold">
+              No
+            </label>
+            <label onClick={()=>handleDelete(order._id)} className="btn btn-warning">
+              Confirm Delete
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DeleteConfirmation;
